04bed48daee5d507cbab048be0f01245698defdc
Cody Barr <cody.barr@gmail.com>
2019-11-19 19:09:27 -0500

    Typo

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -29,7 +29,7 @@ function SearchForm({ updateResults, updateLoading }) {
 		}
 
 		if (selectedBookType !== 'none') {
-			params.filter = selectedPrintType
+			params.filter = selectedBookType
 		}
 
 		return Object.keys(params)
